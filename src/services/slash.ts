1777263e3d717ee594caf2d77fc1c5081f316d83
Pedro Luis <pedrolcr@outlook.com>
2021-12-28 17:08:15 -0300

    v1.3.2

--- src/services/slash.ts
+++ src/services/slash.ts
@@ -1,9 +1,32 @@
 import { Routes } from 'discord-api-types/v9';
-import { SlashCommandBuilder } from '@discordjs/builders';
+import {
+  SlashCommandBooleanOption,
+  SlashCommandBuilder,
+  SlashCommandChannelOption,
+  SlashCommandMentionableOption,
+  SlashCommandNumberOption,
+  SlashCommandRoleOption,
+  SlashCommandStringOption,
+  SlashCommandSubcommandBuilder,
+  SlashCommandUserOption,
+} from '@discordjs/builders';
+import _, { assign } from 'lodash';
 import { ICommand } from '../interfaces/ICommand';
 import { discordRest } from './rest';
 import logger from '../services/logger';
-import { recursiveMergeArrayBy } from '../utils/utils';
+import { OasisError } from '..';
+
+function recursiveMergeArrayBy(array: Array<unknown>, attribute: string) {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  return _.uniqWith(array, (pre: any, cur: any) => {
+    if (pre[attribute] === cur[attribute]) {
+      const mergedOptions = cur.options.concat(pre.options);
+      cur.options = recursiveMergeArrayBy(mergedOptions, attribute);
+      return true;
+    }
+    return false;
+  });
+}
 
 function parseCommand(command: ICommand): unknown {
   const fullSplittedCommandName = command.name.split(' ');
@@ -13,20 +36,56 @@ function parseCommand(command: ICommand): unknown {
   commandData.setName(commandName);
   commandData.setDescription(command.description);
 
+  let subCommandRef: SlashCommandBuilder | SlashCommandSubcommandBuilder | null = null;
+
   if (subCommandGroupName && subCommandName) {
     commandData.addSubcommandGroup((subCommandGroup) =>
       subCommandGroup
         .setName(subCommandGroupName)
         .setDescription(command.description)
-        .addSubcommand((subCommand) => subCommand.setName(subCommandName).setDescription(command.description)),
+        .addSubcommand((subCommand) => {
+          subCommand.setName(subCommandName).setDescription(command.description);
+          subCommandRef = subCommand;
+          return subCommand;
+        }),
     );
   } else if (subCommandGroupName) {
-    commandData.addSubcommand((subCommand) =>
-      subCommand.setName(subCommandGroupName).setDescription(command.description),
-    );
+    commandData.addSubcommand((subCommand) => {
+      subCommand.setName(subCommandGroupName).setDescription(command.description);
+      subCommandRef = subCommand;
+      return subCommand;
+    });
   } else {
-    Object.assign(commandData.options, ...command.options);
+    subCommandRef = commandData;
   }
+
+  command.options.forEach((option) => {
+    switch (option.type) {
+      case 'BOOLEAN':
+        subCommandRef?.addBooleanOption(assign(new SlashCommandBooleanOption(), option));
+        break;
+      case 'NUMBER':
+        subCommandRef?.addNumberOption(assign(new SlashCommandNumberOption(), option));
+        break;
+      case 'STRING':
+        subCommandRef?.addStringOption(assign(new SlashCommandStringOption(), option));
+        break;
+      case 'MENTIONABLE':
+        subCommandRef?.addMentionableOption(assign(new SlashCommandMentionableOption(), option));
+        break;
+      case 'USER':
+        subCommandRef?.addUserOption(assign(new SlashCommandUserOption(), option));
+        break;
+      case 'CHANNEL':
+        subCommandRef?.addChannelOption(assign(new SlashCommandChannelOption(), option));
+        break;
+      case 'ROLE':
+        subCommandRef?.addRoleOption(assign(new SlashCommandRoleOption(), option));
+        break;
+      default:
+        throw new OasisError('Argument type not supported');
+    }
+  });
 
   return commandData.toJSON();
 }
