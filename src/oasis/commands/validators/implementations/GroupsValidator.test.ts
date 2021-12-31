5f931b35e5a1933035ee3c6835effbe7b969b6e9
Gabriel Carvalho <braga.gabrielcarvalho@gmail.com>
2021-12-31 13:00:35 -0300

    (FIX) GroupsValidator test covering 100% of tests
    Jest wasnt the actual problem at the code, actually the problem was how we were dealing with the throw exception

--- src/oasis/commands/validators/implementations/GroupsValidator.test.ts
+++ src/oasis/commands/validators/implementations/GroupsValidator.test.ts
@@ -1,31 +1,62 @@
 import { Message } from 'discord.js';
+import { CommandError } from '../../error/CommandError';
 import { GroupsValidator } from './GroupsValidator';
 
 describe('Validate groups', () => {
   const validator = new GroupsValidator();
 
-  describe('DM Only command', () => {
-    describe('inside GUILD_TEXT channel', () => {
-      const command = {
-        commandHolder: {
-          group: 'dmOnly',
-          args: [],
-          options: [
-            {
-              required: true,
-            },
-          ],
-        },
-        channel: {
-          send: jest.fn(),
-          type: 'GUILD_TEXT',
-        },
-      } as unknown as Message;
+  describe('inside GUILD_TEXT channel', () => {
+    const command = {
+      commandHolder: {
+        group: 'dmOnly',
+        args: [],
+        options: [
+          {
+            required: true,
+          },
+        ],
+      },
+      channel: {
+        send: jest.fn(),
+        type: 'GUILD_TEXT',
+      },
+    } as unknown as Message;
 
-      it('should fail validation', async () => {
-        await validator.validate(command);
-        expect(1).toBe(1);
-        // expect(command.channel.send).toHaveBeenCalled();
+    describe('DM only command', () => {
+      it('should fail validation', () => {
+        validator.validate(command).catch((error) => {
+          expect(error).not.toBeInstanceOf(Error);
+          expect(error).toBeInstanceOf(CommandError);
+          expect(error.message).toBe('You can only use this command inside dms');
+        });
+      });
+    });
+  });
+
+  describe('inside DM channel', () => {
+    const command = {
+      commandHolder: {
+        group: 'guildOnly',
+        args: [],
+        options: [
+          {
+            required: true,
+          },
+        ],
+      },
+      channel: {
+        send: jest.fn(),
+        type: 'DM',
+      },
+    } as unknown as Message;
+
+    describe('DM only command', () => {
+      it('should fail validation', () => {
+        validator.validate(command).catch((error) => {
+          expect(error).not.toBeInstanceOf(Error);
+          expect(error).toBeInstanceOf(CommandError);
+          expect(error.message).toBe('You can only use this command inside a server');
+        });
       });
     });
   });
